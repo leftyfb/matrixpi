e438f6b5753c43e342126ead9434e6b8555dd794
Mike Rushton <mike@mikerushton.com>
2023-03-16 21:15:10 -0400

    updated variables clean up logging a bit turn off screen at night as opposed to just dimming

--- led_script.bash
+++ led_script.bash
@@ -4,8 +4,10 @@ api_key=
 zipcode=
 timeout=10
 prev_hour=0
-images="/home/pi/images/"
-weather_icons="/home/pi/weather_icons"
+home=/home/pi/
+matrixpi=$home/matrixpi
+images="$matrixpi/images/"
+weather_icons="/$matrixpi/weather_icons"
 
 [ ! -d $images ] && mkdir $images
 [ ! -d $weather_icons ] && mkdir $weather_icons
@@ -13,7 +15,7 @@ weather_icons="/home/pi/weather_icons"
 # our main command to send pictures and animations for the LED panel
 ledmatrix(){
 	sleep .1
-	timeout $timeout /home/pi/rpi-rgb-led-matrix/utils/led-image-viewer --led-gpio-mapping=adafruit-hat --led-brightness=$brightness -C $@
+	timeout $timeout $home/rpi-rgb-led-matrix/utils/led-image-viewer --led-gpio-mapping=adafruit-hat --led-brightness=$brightness -C $@
 }
 
 weather(){
@@ -22,8 +24,8 @@ weather(){
 	[ -v $zipcode ] && return 0
         # grab temperature and weather icon every hour
         if [ $cur_hour != $prev_hour ] && (( $cur_hour % 3 == 0 )) ; then
-		[ -f $weather_icons/temperature.png ] && rm $weather_icons/temperature.png
-		[ -f $weather_icons/out.png ] && rm $weather_icons/out.png
+		[ -f $weather_icons/temperature.png ] && rm $weather_icons/temperature.png 2>/dev/null
+		[ -f $weather_icons/out.png ] && rm $weather_icons/out.png 2>/dev/null
                 echo "time changed .... time to do things ...."
                 read temp icon < <(echo $(curl -s "http://api.weatherstack.com/current?access_key=${api_key}&query=${zipcode}&units=f" | jq -r '.current.temperature, .current.weather_icons[0]'))
                 icon_filename=$(basename $(echo $icon))
@@ -34,8 +36,11 @@ weather(){
 		# overlay temperature on downloaded weather icon
 		convert -background '#0009' -fill white -stroke white -pointsize 22 +antialias label:"${temp}F" -gravity South -size 64,64 $weather_icons/temperature.png
 		convert -composite -geometry +30+45 $weather_icons/$icon_filename $weather_icons/temperature.png $weather_icons/out.png
+	else
+			echo "we only run every 3 hours"
         fi
         timeout=10
+	echo "display out.png"
         ledmatrix $weather_icons/out.png
 }
 
@@ -58,12 +63,12 @@ digdug(){
 while true; do
 	cur_hour=$(date +%H)
 	funcs=$(compgen -A function|egrep -v "pi|ledmatrix|weather"|sort -R)
-	echo $funcs
 	for animation in $funcs ; do
 		hour=$(date +%H)
 		# dim LED to 30% between 11pm and 5am
-		if [ "$hour" -ge "23" ] || [ "$hour" -lt "5" ] ;then
-			brightness=30
+		if [ "$hour" -ge "23" ] || [ "$hour" -lt "7" ] ;then
+			sleep 10
+			break
 		else
 			brightness=100
 		fi
